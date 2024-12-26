09dbdf4c2e4a20597bd09769e396eff370e4aaa2
krishnanandiraju <krishnanandiraju@gmail.com>
2024-12-26 10:48:03 +0530

    Removing the redundant codes

--- forms/connect_with_tsc.php
+++ forms/connect_with_tsc.php
@@ -15,17 +15,17 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $book_a_table = new PHP_Email_Form;
-  $book_a_table->ajax = true;
+  $connect_with_tsc= new PHP_Email_Form;
+  $connect_with_tsc->ajax = true;
   
-  $book_a_table->to = $receiving_email_address;
-  $book_a_table->from_name = $_POST['name'];
-  $book_a_table->from_email = $_POST['email'];
-  $book_a_table->subject = "New table booking request from the website";
+  $connect_with_tsc->to = $receiving_email_address;
+  $connect_with_tsc->from_name = $_POST['name'];
+  $connect_with_tsc->from_email = $_POST['email'];
+  $connect_with_tsc->subject = "New table booking request from the website";
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
-  $book_a_table->smtp = array(
+  $connect_with_tsc->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
@@ -33,13 +33,13 @@
   );
   */
 
-  $book_a_table->add_message( $_POST['name'], 'Name');
-  $book_a_table->add_message( $_POST['email'], 'Email');
-  $book_a_table->add_message( $_POST['phone'], 'Phone', 4);
-  $book_a_table->add_message( $_POST['date'], 'Date', 4);
-  $book_a_table->add_message( $_POST['time'], 'Time', 4);
-  $book_a_table->add_message( $_POST['people'], '# of people', 1);
-  $book_a_table->add_message( $_POST['message'], 'Message');
+  $connect_with_tsc->add_message( $_POST['name'], 'Name');
+  $connect_with_tsc->add_message( $_POST['email'], 'Email');
+  $connect_with_tsc->add_message( $_POST['phone'], 'Phone', 4);
+  $connect_with_tsc->add_message( $_POST['date'], 'Date', 4);
+  $connect_with_tsc->add_message( $_POST['time'], 'Time', 4);
+  $connect_with_tsc->add_message( $_POST['people'], '# of people', 1);
+  $connect_with_tsc->add_message( $_POST['message'], 'Message');
 
-  echo $book_a_table->send();
+  echo $connect_with_tsc->send();
 ?>
